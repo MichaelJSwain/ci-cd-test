283f83dff1f7f52afadef83d37f0724add96ad56
Michael Swain <michaelswain@tommy.com>
2025-07-21 15:40:37 +0200

    add bug in getListItems util func

--- src/utils/getListItems.js
+++ src/utils/getListItems.js
@@ -1,3 +1,3 @@
 export const getListItems = () => {
-    return [{id: 1, name: 'item 1'}, {id: 2, name: 'item 2'}, {id: 3, name: 'item 3'}, {id: 4, name: 'item 4'}];
+    return [{id: 1, name: 'item 1'}, {id: 2, name: 'item 2'}, {id: 3, name: 'item 3'}, {id: 4, name: 'item 5'}];
 }
