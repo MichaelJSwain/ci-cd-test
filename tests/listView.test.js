ddce5d085c6834651e7ba481ad4f7b6ba59696ff
Michael Swain <michaelswain@tommy.com>
2025-07-22 08:17:54 +0200

    fix handleAddItem func

--- tests/listView.test.js
+++ tests/listView.test.js
@@ -4,7 +4,7 @@ import { getListItems } from "../src/utils/getListItems";
 describe("get list items", () => {
     it("should return an array of list items", () => {
         // arrange
-        const expectedResult = [{id: 1, name: 'item 1'}, {id: 2, name: 'item 2'}, {id: 3, name: 'item 3'}, {id: 4, name: 'item 4'}];
+        const expectedResult = [{id: 1, name: 'item 2'}, {id: 2, name: 'item 2'}, {id: 3, name: 'item 3'}, {id: 4, name: 'item 4'}];
 
         // act
         const result = getListItems();
